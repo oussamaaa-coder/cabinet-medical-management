8e544cbb198cb2f6340d3f4440825ca4d9d15b08
Mohamed Ali <mohamedalitkhsaoui@gmail.com>
2026-03-05 12:01:33 +0000

    Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\PatientController;
+use App\Http\Controllers\AppointmentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Routes pour les doctors
+Route::resource('doctors', DoctorController::class);
+
+// Routes pour les patients
+Route::resource('patients', PatientController::class);
+
+// Routes pour les appointments
+Route::resource('appointments', AppointmentController::class);
